36a748a68383cf7813ac58a233060ed84739a46a
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-03 17:06:19 +0900

    key 보안

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDBJgCxr6wk_xe7Gn93-zJSnRZ7fkmTKwI",
-    authDomain: "nwitter-7c435.firebaseapp.com",
-    // databaseURL: "https://nwitter-7c435.firebaseio.com",
-    projectId: "nwitter-7c435",
-    storageBucket: "nwitter-7c435.appspot.com",
-    messagingSenderId: "963294864440",
-    appId: "1:963294864440:web:6dcba7014276f483cab2cd"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
