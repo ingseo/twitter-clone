573103d4fc53409cf8686e5e35b783e493fb8b6f
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-05 04:17:26 +0900

    profile 화면 연결 및 로그아웃 기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,24 +2,32 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
     //Router가 하나의 용도로만 쓰여야하기때문에 useState는 App.js로 이동. Routs만 보이게 한다.
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
+            {/* Navigation이 존재하려면 isLoggedIn이 true여야 한다. */}
             <Switch>
                 {/* 로그인이 되어있다면, home으로, 아니라면 Auth로 이동 */}
                 {isLoggedIn ? (
-                    <> {/* Fragment = 많은 요소들을 render하고 싶을 때 사용. 부모가 없을때 */}
+                    <> 
                         <Route exact path="/"> 
-                        {/* path = 이동하는 주소경로. 이 경우 경로이동없이 자식컴포넌트인 Home을 띄운다. */}
                             <Home />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile />
                         </Route>
                     </> 
                 ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                    </>
                 )}
             </Switch>
         </Router>
