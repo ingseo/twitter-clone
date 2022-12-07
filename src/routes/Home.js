bc38acdf3b8dc0965a473f30370de2201b5025c0
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-07 23:43:34 +0900

    메인트윗화면 form설정, 데이터베이스셋업

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    const onChange = (event) => {
+        const {
+            target:{ value },
+        } = event; //=from event. event 안의 target안의 value 
+        setNweet(value);
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={nweet} 
+                    onChange={onChange} 
+                    type="text" 
+                    placeholder="What's on your mind?" 
+                    maxLength={140} 
+                />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
+
 export default Home;
