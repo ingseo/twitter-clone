ae14c7300f5c47c7cbd1420d9a028fdbe6ce76a3
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-08 23:12:56 +0900

    collection-doc 이용한 tweet기능구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,21 @@
+import { dbService } from "fBase";
 import React, {useState} from "react";
+import { collection, addDoc } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: Date.now(),
+        });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+        setNweet("");
     }
     const onChange = (event) => {
         const {
