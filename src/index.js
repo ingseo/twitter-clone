a831d471ade0e36554d539ce34cba25414c7db8d
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-09 00:25:21 +0900

    tweet 가져오기

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ console.log(fBase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
 import { dbService } from "fBase";
-import React, {useState} from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async() => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        dbNweets.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setNweets(prev => [nweetObject, ...prev]) 
+        });
+    }
+    useEffect(() => {
+        getNweets();
+    },[])
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -23,6 +37,8 @@ const Home = () => {
         } = event; //=from event. event 안의 target안의 value 
         setNweet(value);
     }
+    console.log(nweets);
+    
     return(
         <div>
             <form onSubmit={onSubmit}>
@@ -35,6 +51,12 @@ const Home = () => {
                 />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet => 
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>)}
+            </div>
         </div>
     )
 }
