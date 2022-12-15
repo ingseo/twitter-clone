4a31329611b198611a257a3db85b75ca31986696
ingseo <74130681+ingseo@users.noreply.github.com>
2022-12-16 01:46:34 +0900

    트윗 수정, 삭제 기능 추가

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,14 +1,69 @@
-import React from "react"; 
+import { dbService } from "fBase";
+import React, { useState } from "react"; 
+import { doc, deleteDoc, updateDoc }from"firebase/firestore";
 
 const Tweet = ({ tweetObj, isOwner }) => {
-    (
+    const TweetTextRef =doc(dbService, "tweets", `${tweetObj.id}`);
+
+    //update(edit)
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+    const toggleEditing = () => setEditing(prev => !prev);
+    const editCancel = () => {
+        toggleEditing();
+        setNewTweet(tweetObj.text);
+    }
+    const onChange = (event) => {
+        const {
+            target : { value },
+        } = event
+        setNewTweet(value);
+    }
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        await updateDoc(TweetTextRef, {
+            text: newTweet,
+        });
+        toggleEditing();
+    }
+
+    //delete
+    const onDeleteClick = async() => {
+        const ok = window.confirm("Are you sure you want to delelte this tweet?");
+        if(ok){
+            await deleteDoc(TweetTextRef);
+        }
+    }
+
+    return (
         <div>
-            <h4>{tweetObj.text}</h4>
-            <span>{new Date(tweetObj.createdAt).toLocaleString()}</span>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button>Delete</button>
-                    <button>Edit</button>
+                    <form onSubmit={onSubmit}>
+                        <input 
+                            type="text" 
+                            value={newTweet} 
+                            placeholder="What's on your mind?"  
+                            required 
+                            onChange={onChange}
+                        />
+                        <input 
+                            type="submit"
+                            value="Update Tweet"
+                        />
+                    </form>
+                    <button onClick={editCancel}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <h4>{tweetObj.text}</h4>
+                    <span>{new Date(tweetObj.createdAt).toLocaleString()}</span>
+                    {isOwner && (
+                        <>
+                            <button onClick={toggleEditing}>Edit</button>
+                            <button onClick={onDeleteClick}>Delete</button>
+                        </>
+                    )}
                 </>
             )}
         </div>
