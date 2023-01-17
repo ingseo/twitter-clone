37ef982faa23f146097d7c3084d898251c0abe57
ingseo <74130681+ingseo@users.noreply.github.com>
2023-01-17 19:36:59 +0900

    트윗 정보에 이미지url 포함

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import { dbService } from "fBase";
+import { dbService, storageService } from "fBase";
 import React, { useEffect, useState, useRef } from "react";
 import { 
     collection, 
@@ -7,12 +7,14 @@ import {
     orderBy,
     query
 } from "firebase/firestore";
+import { v4 as uuidv4 } from 'uuid';
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
 import Tweet from "components/Tweet";
 
 const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
-    const [attachment, setAttachment] = useState();
+    const [attachment, setAttachment] = useState("");
 
     useEffect(() => {
         const q = query(
@@ -29,17 +31,31 @@ const Home = ({ userObj }) => {
     },[])
     const onSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const docRef = await addDoc(collection(dbService, "tweets"), {
+        let attachmentUrl = "";
+        //파일에 대한 레퍼런스를 만들고, 몇몇 내용과 함께 업로드
+        //이미지 첨부하지 않고 텍스트만 올리고 싶을 때도 있기 때문에 attachment가 있을때만 아래 코드 실행
+        //이미지 첨부하지 않은 경우엔 attachmentUrl=""이 된다.
+        if (attachment !== "") {
+            //파일 경로 참조 만들기
+            const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+            //storage 참조 경로로 파일 업로드 하기
+            const response = await uploadString(attachmentRef, attachment, "data_url");
+            //storage 참조 경로에 있는 파일의 URL을 다운로드해서 attachmentUrl 변수에 넣어서 업데이트
+            attachmentUrl = await getDownloadURL(response.ref);
+        }
+        //트윗 오브젝트
+        const tweetObj = {
             text: tweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
-        });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (error) {
-            console.error("Error adding document: ", error);
+            attachmentUrl,
         }
+        //트윗하기 누르면 nweetObj 형태로 새로운 document 생성하여 nweets 콜렉션에 넣기
+        await addDoc(collection(dbService, "tweets"), tweetObj);
+        //state 비워서 form 비우기
         setTweet("");
+        //파일 미리보기 img src 비워주기
+        setAttachment("");
     }
     const onChange = (event) => {
         const {
@@ -63,8 +79,8 @@ const Home = ({ userObj }) => {
     }
     const fileInput = useRef();
     const onClearAttachment = () => {
-        setAttachment(null);
-        fileInput.current.value = null;
+        setAttachment("");
+        fileInput.current.value = "";
     }
     
 
