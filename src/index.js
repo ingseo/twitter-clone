6f4978d8474446da7b57cbaa753337c9b2d3e394
ingseo <inseo37@gmail.com>
2023-04-17 07:32:00 +0900

    css reset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import "style/reset.css"
-import "style/common.css"
-import "style/auth.css"
-import "style/profile.css"
-import "style/tweet.css"
+// import "style/reset.css"
+// import "style/common.css"
+// import "style/auth.css"
+// import "style/profile.css"
+// import "style/tweet.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
