0a107f208541f2ad9c40201be2b70f7bac148b05
ingseo <inseo37@gmail.com>
2023-04-22 06:56:52 +0900

    auth page auth부분 css작업

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,8 @@ import Contact from 'components/Contact';
 import Trend from 'components/Trend';
 import Modal from 'react-modal';
 
+import 'style/scss/auth.scss'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
@@ -17,23 +19,25 @@ const Auth = () => {
                 <Trend />
             </div>
             <div className="authContainer container">
-                <div className="title">
-                    <h1>트위터에 처음이세요?</h1>
-                    <p>지금 가입해서 나에게 맞춤 설정된 타임라인을 만들어 보세요!</p>
-                </div>
-                <div className='btnGroup'>
-                    <AuthBtn />
-                    <button onClick={()=> setModalIsOpen(true)} name="create" className="authBtn">
-                        계정 만들기
-                    </button>
-                    <Modal isOpen={modalIsOpen} ariaHideApp={false} onRequestClose={() => setModalIsOpen(false)}>
-                        <AuthForm popupType={true}/>
-                        <button className="xBtn" onClick={()=> setModalIsOpen(false)}>
-                            <FontAwesomeIcon icon={faTimes} />
+                <div className='authBox'>
+                    <div className="title">
+                        <h1>트위터에 처음이세요?</h1>
+                        <p>지금 가입해서 나에게 맞춤 설정된 타임라인을 만들어 보세요!</p>
+                    </div>
+                    <div className='btnGroup'>
+                        <AuthBtn />
+                        <button onClick={()=> setModalIsOpen(true)} name="create" className="authBtn">
+                            계정 만들기
                         </button>
-                    </Modal>
-                </div>
+                        <Modal isOpen={modalIsOpen} ariaHideApp={false} onRequestClose={() => setModalIsOpen(false)}>
+                            <AuthForm popupType={true}/>
+                            <button className="xBtn" onClick={()=> setModalIsOpen(false)}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </button>
+                        </Modal>
+                    </div>
                 <Contact />
+                </div>
             </div>
         </div>
     );
